c24531e8a813e8ed4e2309fd71570a2f90c8777b
Apiz22 <122146975+Apiz22@users.noreply.github.com>
2023-05-11 09:32:33 +0800

    update

--- source/user_pages/dashboard_page/user-dashboard.js
+++ source/user_pages/dashboard_page/user-dashboard.js
@@ -1,6 +1,7 @@
-const formOpenButton = document.querySelector("#form-open"),
-home = document.querySelector(".home2"),
+const formOpenButton = document.querySelector(".wallet_show"),
+home = document.querySelector(".home"),
+formContainer = document.querySelector(".form_container"),
 formCloseButton = document.querySelector(".form_close");
 
-formOpenButton.addEventListener("click", () => home.classList.add("show"))
-formCloseButton.addEventListener("click", () => home.classList.remove("show"))
+formOpenButton.addEventListener("click", () => home.classList.add("show"));
+formCloseButton.addEventListener("click", () => home.classList.remove("show"));
