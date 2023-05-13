40878c8afd6866c2d0c7428634a99aec7e95a146
Apiz22 <122146975+Apiz22@users.noreply.github.com>
2023-05-13 16:49:40 +0800

    Update user-dashboard.js

--- src/main/webapp/source/user_pages/dashboard_page/user-dashboard.js
+++ src/main/webapp/source/user_pages/dashboard_page/user-dashboard.js
@@ -1,5 +1,6 @@
 const formOpenButton = document.querySelector(".wallet_show"),
 home = document.querySelector(".home"),
+walletMenu = document.getElementById("walletMenu"),
 subMenu = document.getElementById("subMenu"),
 formContainer = document.querySelector(".form_container"),
 formCloseButton = document.querySelector(".form_close");
@@ -8,3 +9,4 @@ formOpenButton.addEventListener("click", () => home.classList.add("show"));
 formCloseButton.addEventListener("click", () => home.classList.remove("show"));
 
 function toggleMenu(){subMenu.classList.toggle("open-menu");}
+function toggleWallet(){subMenu.classList.toggle("open-wallet");}
