48f33051f1d68a35716afda75ee15df33f9a33ac
nznxvir <2021829384@student.uitm.edu.my>
2023-05-30 01:57:41 +0800

    admin dashboard

---
+++ src/main/webapp/source/admin_pages/dashboard_page/admin-dashboard.js
@@ -0,0 +1,11 @@
+// MenuToggle
+toggle = document.querySelector('.toggle');
+nav = document.querySelector('.nav');
+main = document.querySelector('.main-div');
+subMenu = document.getElementById("subMenu"),
+
+toggle.onclick = function(){
+    nav.classList.toggle('active');
+    main.classList.toggle('active');
+}
+function toggleMenu(){subMenu.classList.toggle("open-menu");}
