5ede485a16050d2dfacdfbc29fafb607b060fad4
CokeSodaWine <87024651+CokeSodaWine@users.noreply.github.com>
2023-06-20 00:54:57 +0800

    Update ConfirmBookingController.java

--- src/main/java/system/controller/ConfirmBookingController.java
+++ src/main/java/system/controller/ConfirmBookingController.java
@@ -16,6 +16,14 @@ public class ConfirmBookingController extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String pickupLoc = request.getParameter("pickup_location");
-		System.out.println(pickupLoc);
+		String dropLoc = request.getParameter("drop_location");
+		String pickupDate = request.getParameter("pickup_date");
+		String dropDate = request.getParameter("drop_date");
+		int passengers = Integer.parseInt(request.getParameter("passengers_num"));
+		double rentalChrge = Double.parseDouble(request.getParameter("rental_charge").substring(3));
+		String specialReq = request.getParameter("special_request");
+		
+		System.out.println(pickupLoc + "\n" + dropLoc + "\n" + pickupDate + "\n" + dropDate + "\n" + passengers + "\n" + rentalChrge + "\n" + specialReq);
 	}
 }
+1
