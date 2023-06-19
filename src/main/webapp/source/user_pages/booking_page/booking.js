675b9fc9a148051254bbec98bdf9f224881f8dde
haikalsamari <haikalsamari.gg@gmail.com>
2023-06-19 23:01:36 +0800

    apa

--- src/main/webapp/source/user_pages/booking_page/booking.js
+++ src/main/webapp/source/user_pages/booking_page/booking.js
@@ -1,6 +1,9 @@
 const formOpenButton = document.querySelector(".wallet_show"),
+subMenu = document.getElementById("subMenu"),
 home = document.querySelector(".test1"),
 formCloseButton = document.querySelector(".form_close");
 
 formOpenButton.addEventListener("click", () => home.classList.add("show"));
 formCloseButton.addEventListener("click", () => home.classList.remove("show"));
+
+function toggleMenu(){subMenu.classList.toggle("open-menu");}
